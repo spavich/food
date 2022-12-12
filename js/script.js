cc93df74029b2379a967a39c6dbf2e0ac3c2d318
Sasha Paraskevych <sasha.paraskevych@gmail.com>
2022-12-12 07:44:18 +0200

    feat: add Timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () =>{
+	// Tabs
 	const tabContent = document.querySelectorAll('.tabcontent'),
 			tabsParent = document.querySelector('.tabheader__items'),
 			tabs = tabsParent.querySelectorAll('.tabheader__item');
@@ -34,4 +35,51 @@ window.addEventListener('DOMContentLoaded', () =>{
 			});
 		}
 	});
+
+	// Timer
+	const deadline = Date.parse('2023.09.23');
+
+	const getTimeRemaining = (endTime) =>{
+		const total = endTime - Date.parse(new Date());
+
+		const days = Math.floor(total / (1000 * 60 * 60 * 24));
+		const hours = Math.floor(total / (1000 * 60 * 60) %24);
+		const minutes = Math.floor(total / (1000 * 60) %60);
+		const seconds = Math.floor(total / (1000) %60);
+
+		return{
+			total,
+			days,
+			hours,
+			minutes,
+			seconds
+		}
+	};
+
+	const getZero = (num) =>{
+		return (0 <= num && num < 10) ? `0${num}` : num;
+	};
+	const setClock = (selector, endTime) =>{
+		const timer = document.querySelector(selector),
+					days = timer.querySelector('#days'),
+					minutes = timer.querySelector('#minutes'),
+					seconds = timer.querySelector('#seconds');
+		const timeInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+		function updateClock (){
+			const t = getTimeRemaining(endTime)
+
+			days.textContent = getZero(t.days);
+			minutes.textContent = getZero(t.minutes);
+			seconds.textContent = getZero(t.seconds);
+
+			if(t.total <= 0){
+				clearInterval(timeInterval);
+			}
+		}
+	};
+
+	setClock('.timer', deadline);
+
 });
