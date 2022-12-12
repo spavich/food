8c5b0b2a3a4e812e21fb8ee84a7d837eb4da9aeb
Sasha Paraskevych <sasha.paraskevych@gmail.com>
2022-12-12 17:25:22 +0200

    feat: add MenuCards

--- js/script.js
+++ js/script.js
@@ -124,4 +124,65 @@ window.addEventListener('DOMContentLoaded', () =>{
 			closeModal();
 		}
 	});
+
+	// Cards
+	const menuElement = document.querySelector('.menu__field .container');
+
+	class MenuCard{
+		constructor(src, alt, title, descr, price, parentSelector, ...props){
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.props = props;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 45;
+			this.changeToUAH();
+		}
+		changeToUAH(){
+			this.price = this.price *  this.transfer;
+		}
+		render(){
+			const element = document.createElement('div');
+			(this.props.length === 0) ? element.classList.add('menu__item') : this.props.forEach(item => element.classList.add(item));
+
+			element.innerHTML = `
+					<img src=${this.src} alt=${this.alt}>
+					<h3 class="menu__item-subtitle">${this.title}</h3>
+					<div class="menu__item-descr">${this.descr}</div>
+					<div class="menu__item-divider"></div>
+					<div class="menu__item-price">
+							<div class="menu__item-cost">Цена:</div>
+							<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+					</div>
+				`
+			this.parent.append(element);
+		}
+	}
+
+	new MenuCard(
+		'img/tabs/vegy.jpg', 
+		'vegy', 
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+		'12',
+		'.menu .container',
+	).render();
+	new MenuCard(
+		'img/tabs/elite.jpg', 
+		'elite', 
+		'Меню “Премиум”',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		'23',
+		'.menu .container',
+	).render();
+	new MenuCard(
+		'img/tabs/post.jpg', 
+		'post', 
+		'Меню "Постное"',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		'15',
+		'.menu .container',
+	).render();
 });
