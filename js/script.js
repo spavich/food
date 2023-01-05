26d9fa06c4239fe939835b371d0c1022f62cd670
Sasha Paraskevych <sasha.paraskevych@gmail.com>
2023-01-05 23:14:27 +0200

    feat: rewrote Promise to AsyncAwait, add db.json file with cards data and submit data forms

--- js/script.js
+++ js/script.js
@@ -157,30 +157,20 @@ window.addEventListener('DOMContentLoaded', () =>{
 		}
 	}
 
-	new MenuCard(
-		'img/tabs/vegy.jpg', 
-		'vegy', 
-		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		'12',
-		'.menu .container',
-	).render();
-	new MenuCard(
-		'img/tabs/elite.jpg', 
-		'elite', 
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		'23',
-		'.menu .container',
-	).render();
-	new MenuCard(
-		'img/tabs/post.jpg', 
-		'post', 
-		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-		'15',
-		'.menu .container',
-	).render();
+	const getResource = async (url) =>{
+		const res = await fetch(url);
+		if(!res.ok){
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
+		return await res.json();
+	};
+
+	getResource('http://localhost:3000/menu')
+	.then(data => {
+		data.forEach(({img, altimg, title, descr, price}) => {
+			new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+		});
+	});
 
 	// Sumbit Data Forms
 	const forms = document.querySelectorAll('form');
@@ -195,6 +185,17 @@ window.addEventListener('DOMContentLoaded', () =>{
 		failure: 'Что-то пошло не так'
 	};
 
+	const postData = async (url, data) =>{
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: data
+		});
+		return await res.json();
+	};
+
 	function sumbitData(form){
 		form.addEventListener('submit', (e) =>{
 			e.preventDefault();
@@ -205,18 +206,9 @@ window.addEventListener('DOMContentLoaded', () =>{
 			form.insertAdjacentElement('afterend', messageStatus);
 
 			const formData = new FormData(form);
-			const object = {};
-			formData.forEach((item, i) =>{
-				object[i] = item;
-			});
+			const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-			fetch('server.php', {
-				method: 'POST',
-				headers: {
-					'Content-type': 'application/json'
-				},
-				body: JSON.stringify(object)
-			}).then(data => data.text())
+			postData('http://localhost:3000/requests', json)
 			.then(data => {
 				console.log(data);
 				showThanksModal(messageUser.success);
